5008a5fd132bc8096a64386fe4a28e1f2bc6d946
Valeria <valeriashilkovsk@gmail.com>
2019-11-29 14:04:39 +0300

    add presenter

---
+++ SuperCalculator/Presenter/CalculatorPresenter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using SuperCalculator.Model;
+using SuperCalculator.Service;
+
+namespace SuperCalculator.Presenter
+{
+    public class CalculatorPresenter
+    {
+        private readonly CalculatorView calculatorView;
+        private readonly CalculatorService calculatorService;
+
+        public CalculatorPresenter(CalculatorView calculatorView, CalculatorService calculatorService)
+        {
+            this.calculatorView = calculatorView;
+            this.calculatorService = calculatorService;
+
+            calculatorView.SetData += SetNumbers;
+            calculatorView.OperationExecuteEvent += ExecuteOperationExecute;
+            calculatorView.OperationRedoEvent += Redo;
+            calculatorView.OperationUndoEvent += Undo;
+        }
+
+        private void Undo()
+        {
+            List<BigInteger> numbers = calculatorService.Undo();
+            calculatorService.SetData(numbers);
+            calculatorView.UpdateNumbers(numbers);
+            calculatorView.updateAllOperations(calculatorService.GetAllOperationsCount());
+            calculatorView.updateCurrentOperation(calculatorService.GetCurrentOperation());
+        }
+        
+        private void Redo()
+        {
+            List<BigInteger> numbers = calculatorService.Redo();
+            calculatorService.SetData(numbers);
+            calculatorView.UpdateNumbers(numbers);
+            calculatorView.updateAllOperations(calculatorService.GetAllOperationsCount());
+            calculatorView.updateCurrentOperation(calculatorService.GetCurrentOperation());
+        }
+
+        private void SetNumbers(List<BigInteger> numbers)
+        {
+            calculatorService.SetData(numbers);
+            calculatorView.UpdateNumbers(numbers);
+        }
+
+        private void ExecuteOperationExecute(Operation operation)
+        {
+            List<BigInteger> numbers = calculatorService.Execute(operation);
+            calculatorService.SetData(numbers);
+            calculatorView.UpdateNumbers(numbers);
+            calculatorView.updateAllOperations(calculatorService.GetAllOperationsCount());
+            calculatorView.updateCurrentOperation(calculatorService.GetCurrentOperation());
+        }
+    }
+}
