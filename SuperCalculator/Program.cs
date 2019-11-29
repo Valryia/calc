dea37bbd3858af5f6574b2b02a3692cf9280ee7b
Valeria <valeriashilkovsk@gmail.com>
2019-11-29 14:04:15 +0300

    add program

---
+++ SuperCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ninject;
+using SuperCalculator.Presenter;
+using SuperCalculator.Service;
+
+namespace SuperCalculator
+{
+    static class Program
+    {
+        static Ninject.StandardKernel kernel = new StandardKernel();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            kernel.Bind<CalculatorView>()
+                .ToConstant(new CalculatorView())
+                .InSingletonScope();
+            kernel.Bind<CalculatorService>()
+                .ToConstant(new CalculatorService())
+                .InSingletonScope();
+            kernel.Bind<CalculatorPresenter>()
+                .ToConstant(new CalculatorPresenter(kernel.Get<CalculatorView>(), kernel.Get<CalculatorService>()))
+                .InSingletonScope();
+            Application.Run(kernel.Get<CalculatorView>());
+        }
+    }
+}
